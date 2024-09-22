add226ab23c52c2ae28f55bfea052c16abc0d022
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-09-22 07:51:38 +0500

    User Douglas O'Connor saved changes to main.py at 09/22/2024, 02:51:37 UTC time.

--- efac71e9-14ad-40c1-8d41-0776456db50b/main.py
+++ efac71e9-14ad-40c1-8d41-0776456db50b/main.py
@@ -16,32 +16,14 @@ class TradingStrategy(Strategy):
         holdings = data["holdings"]
         data = data["ohlcv"]
 
-        sqqq_stake = 0
-        tqqq_stake = 0
+        sqqq_stake = 50
+        tqqq_stake = 50
 
-        tqqq_bbands = BB("TQQQ", data, 20, 1.4)
         tqqq_ma = SMA("TQQQ", data, 5)
-
-        if len(data)<20:
-            return TargetAllocation({})
-
         current_price = data[-1]["TQQQ"]['close']
 
-        if tqqq_bbands is not None and current_price < tqqq_bbands['lower'][-1] and tqqq_ma[-1]>tqqq_ma[-2]:
-            log("going long")
-            if holdings["TQQQ"] >= 0:
-                tqqq_stake = min(1, holdings["TQQQ"]+0.1)
-            else:
-                tqq_stake = 0.4
-        elif tqqq_bbands is not None and current_price > tqqq_bbands['upper'][-1]:
-            log("going short")
-            if holdings["QQQ"] >= 0:
-                qqq_stake = min(1, holdings["QQQ"]+0.075)
-            else:
-                qqq_stake = 0.2
-        else:
-            log("meh")
-            tqqq_stake = 0
-            qqq_stake = 0
+        print("moving_average", tqqq_ma)
+        print("current_price", current_price)
+
 
         return TargetAllocation({"SQQQ": sqqq_stake, "QQQ": qqq_stake})
