7e9f64bf55393d49b5390336e4facb0027e64eec
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-09-22 08:33:58 +0500

    User Douglas O'Connor saved changes to main.py at 09/22/2024, 03:33:57 UTC time.

--- efac71e9-14ad-40c1-8d41-0776456db50b/main.py
+++ efac71e9-14ad-40c1-8d41-0776456db50b/main.py
@@ -19,10 +19,22 @@ class TradingStrategy(Strategy):
         qqq_stake = 50
         tqqq_stake = 50
 
-        tqqq_ma = SMA("TQQQ", data, 5)
+        tqqq_ma = SMA("TQQQ", data, 30)
         current_price = data[-1]["TQQQ"]['close']
+        moving_average_30 = tqqq_ma[len(tqqq_ma)-1])
+
+        sma_10 = SMA("TQQQ", data, 10)
+        moving_average_10 = sma_10[len(sma_10)-1]
+
 
         log("moving_average: " + str(tqqq_ma[len(tqqq_ma)-1]))
         log("current_price: " + str(current_price))
+
+        if moving_average_10 > moving_average_30:
+            qqq_stake = 50
+            tqqq_stake = 50
+        else:
+            qqq_stake = 70
+            tqqq_stake = 30
 
         return TargetAllocation({"TQQQ": tqqq_stake, "QQQ": qqq_stake})
